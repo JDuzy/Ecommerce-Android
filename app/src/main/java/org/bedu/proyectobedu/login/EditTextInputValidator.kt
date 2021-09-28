d7070f788dbc93521637d2e8d8069f2aa48b5bf7
Juan Duzac <duzacj@gmail.com>
2021-09-28 20:14:12 -0300

    fix: products.json image url broken

--- app/src/main/java/org/bedu/proyectobedu/login/EditTextInputValidator.kt
+++ app/src/main/java/org/bedu/proyectobedu/login/EditTextInputValidator.kt
@@ -5,6 +5,7 @@ package org.bedu.proyectobedu.login
 import com.google.android.material.textfield.TextInputEditText
 
 class EditTextInputValidator {
+
     companion object{
         //Validates if given EditText is not empty, if it is shows the given error msg and return false
          fun validateEditText(editText: TextInputEditText, errorMsg: String) : Boolean{
